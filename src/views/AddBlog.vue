362bab75ad4dfe01481e8520735d2699794d2415
odwanodada <odwanodada22@gmail.com>
2021-08-30 16:11:15 +0200

    Vueblog

---
+++ src/views/AddBlog.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="addBlog"></div>
+  <form @submit.prevent="blogAdd" method="post">
+    <label>Title:</label>
+    <input type="title" required v-model="title" />
+
+    <label>Author:</label>
+    <input type="author" required v-model="author" />
+
+    <label>Image:</label>
+    <input type="img" required v-model="img" />
+
+    <label>Content:</label>
+    <textarea type="content" required v-model="content"> </textarea>
+
+    <button type="submit" class="btn">Create an Blog</button>
+ 
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddBlog",
+  data() {
+    return {
+      title: null,
+      author: null,
+      img: null,
+      content: null,
+    };
+  },
+  methods: {
+    blogAdd() {
+      fetch("http://localhost:5000/add-blog", {
+        method: "POST",
+        body: JSON.stringify({
+          title: this.title,
+          author: this.author,
+          img: this.img,
+          content: this.content,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=utf-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: grey;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+
+label {
+  color: black;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.8em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+
+input[type="checkbox"] {
+  display: inline-block;
+  width: 20px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 3px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+
+.btn {
+  text-align: center;
+}
+
+.error {
+  color: greenyellow;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
+
+input[type="text"].textarea {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  height: 30px;
+}
+</style>
